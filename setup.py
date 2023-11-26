514ad7845dd3adc2757197b24c49870d78cfbf03
Ashzad Kamal <78037204+ashzad123@users.noreply.github.com>
2023-11-26 18:26:19 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Ashmi',
+    author_email= 'ak1503@srmist.edu.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
